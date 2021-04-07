68fa7343c1c26562282f39f10adb1a394f39ca1c
Joshi <Joshi.Gaurav@bcgplatinion.com>
2021-04-07 20:32:34 +0200

    ext updated

--- Samples/Superstore/superstore.js
+++ Samples/Superstore/superstore.js
@@ -2,10 +2,18 @@
 
 // Wrap everything in an anonymous function to avoid poluting the global namespace
 (function() {
+
+  let removeEventListener;
+
   // Use the jQuery document ready signal to know when everything has been initialized
   $(document).ready(function() {
     tableau.extensions.initializeAsync({'configure': showChooseSheetDialog}).then(function() {
-      showChooseSheetDialog();
+      const savedSheetName = tableau.extensions.settings.get('sheet');
+      if (savedSheetName) {
+        loadSelectedMarks(savedSheetName);
+      } else {
+        showChooseSheetDialog();
+      }
     });
 
     function showChooseSheetDialog() {
@@ -15,19 +23,36 @@
       const worksheetNames = worksheets.map((worksheet) => {
         return worksheet.name;
       });
-      demoHelpers.showDialog(dashboardName, worksheetNames, loadSelectedMarks);
+      demoHelpers.showDialog(dashboardName, worksheetNames, saveSheetAndLoadSelectedMarks);
     }
 
 
     function loadSelectedMarks(worksheetName) {
+
+      if (removeEventListener) {
+        removeEventListener();
+      }
+
       $('#selected_marks_title').text(worksheetName);
       const worksheet = demoHelpers.getSelectedSheet(worksheetName);
     
       worksheet.getSelectedMarksAsync().then((marks) => {
           demoHelpers.populateDataTable(marks);
       });
+
+
+      const marksSelectedEventHandler = (event) => {
+        loadSelectedMarks(worksheetName);
+      }
+      removeEventListener = worksheet.addEventListener(
+        tableau.TableauEventType.MarkSelectionChanged, marksSelectedEventHandler);
     }
     
+    function saveSheetAndLoadSelectedMarks(worksheetName) {
+      tableau.extensions.settings.set('sheet', worksheetName);
+      tableau.extensions.settings.saveAsync();
+      loadSelectedMarks(worksheetName);
+    }
   });
 
 })();
